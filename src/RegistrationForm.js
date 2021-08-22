8d76028036a8b814a791d9239945a7042c3cb2ff
Patrick <ptrckxforcadela@gmail.com>
2021-08-23 03:10:40 +0800

    Added more fields in user registration form

--- src/RegistrationForm.js
+++ src/RegistrationForm.js
@@ -24,13 +24,57 @@ export const RegistrationForm = ({ user, onSubmit }) => {
         onSubmit(data)
     })
     return (
-        <form onSubmit={submitHandler}>
-            <div className="form-group">
-                <label htmlFor="firstName">First name:</label>
+        <form onSubmit={submitHandler} class="row g-3">
+            <div className="col-md-4">
+                <label htmlFor="firstName" className="form-label">First name</label>
                 <input {...register('firstName')} className="form-control" type="text" name="firstName" id="firstName" />
-
-                <label htmlFor="lastName">Last name:</label>
+            </div>
+            <div className="col-md-4">
+                <label htmlFor="middleName" className="form-label">Middle name</label>
+                <input {...register('middleName')} className="form-control" type="text" name="middleName" id="middleName" />
+            </div>
+            <div className="col-md-4">
+                <label htmlFor="lastName" className="form-label">Last name</label>
                 <input {...register('lastName')} className="form-control" type="text" name="lastName" id="lastName" />
+            </div>
+            <div className="col-md-6">
+                <label htmlFor="inputEmail4" className="form-label">Email</label>
+                <input type="email" className="form-control" id="inputEmail4" />
+            </div>
+            <div className="col-md-6">
+                <label htmlFor="inputPassword4" className="form-label">Password</label>
+                <input type="password" className="form-control" id="inputPassword4" />
+            </div>
+            <div className="col-12">
+                <label htmlFor="inputAddress" className="form-label">Address</label>
+                <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" />
+            </div>
+            <div className="col-12">
+                <label htmlFor="inputAddress2" className="form-label">Address 2</label>
+                <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
+            </div>
+            <div className="col-md-6">
+                <label htmlFor="inputCity" className="form-label">City</label>
+                <input type="text" className="form-control" id="inputCity" />
+            </div>
+            {/* <div class="col-md-4">
+    <label for="inputState" class="form-label">State</label>
+    <select id="inputState" class="form-select">
+      <option selected>Choose...</option>
+      <option>...</option>
+    </select>
+  </div> */}
+            <div className="col-md-2">
+                <label htmlFor="inputZip" className="form-label">Zip</label>
+                <input type="text" className="form-control" id="inputZip" />
+            </div>
+            <div className="col-12">
+                <div className="form-check">
+                    <input className="form-check-input" type="checkbox" id="gridCheck" />
+                    <label className="form-check-label" for="gridCheck">
+                        Check me out
+                    </label>
+                </div>
             </div>
             <div className="form-group">
                 <Button
