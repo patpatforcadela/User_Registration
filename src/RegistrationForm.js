6d33c821d814f241ff8ae16caf11fe5cfe21b19b
Patrick <ptrckxforcadela@gmail.com>
2021-08-23 23:11:58 +0800

    Added a validation in reg form

--- src/RegistrationForm.js
+++ src/RegistrationForm.js
@@ -3,6 +3,7 @@ import { useForm } from 'react-hook-form'
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import SaveIcon from '@material-ui/icons/Save';
+import { ErrorMessage } from "@hookform/error-message";
 import './style.css'
 
 const useStyles = makeStyles((theme) => ({
@@ -13,7 +14,8 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export const RegistrationForm = ({ user, onSubmit }) => {
-    const { register, handleSubmit } = useForm({
+    const { register, formState: { errors }, handleSubmit } = useForm({
+        criteriaMode: "all",
         defaultValues: {
             firstName: user ? user.firstName : "",
             middleName: user ? user.middleName : "",
@@ -38,32 +40,58 @@ export const RegistrationForm = ({ user, onSubmit }) => {
             <form onSubmit={submitHandler} className="row g-3">
                 <div className="col-md-4">
                     <label htmlFor="firstName" className="form-label">First name</label>
-                    <input {...register('firstName')} className="form-control" type="text" name="firstName" id="firstName" />
+                    <input {...register('firstName', { required: "*First name is required." })} className="form-control" type="text" name="firstName" id="firstName" placeholder="Enter your First name" />
+                    <ErrorMessage
+                        errors={errors}
+                        name="firstName"
+                        render={({ message }) => <p className="error-message">{message}</p>}
+                    />
                 </div>
                 <div className="col-md-4">
                     <label htmlFor="middleName" className="form-label">Middle name</label>
-                    <input {...register('middleName')} className="form-control" type="text" name="middleName" id="middleName" />
+                    <input {...register('middleName', { required: "*Middle name is required." })} className="form-control" type="text" name="middleName" id="middleName" placeholder="Enter your Middle name" />
+                    <ErrorMessage
+                        errors={errors}
+                        name="middleName"
+                        render={({ message }) => <p className="error-message">{message}</p>}
+                    />
                 </div>
                 <div className="col-md-4">
                     <label htmlFor="lastName" className="form-label">Last name</label>
-                    <input {...register('lastName')} className="form-control" type="text" name="lastName" id="lastName" />
+                    <input {...register('lastName', { required: "*Surname is required." })} className="form-control" type="text" name="lastName" id="lastName" placeholder="Enter your Surname" />
+                    <ErrorMessage
+                        errors={errors}
+                        name="lastName"
+                        render={({ message }) => <p className="error-message">{message}</p>}
+                    />
                 </div>
                 <div className="col-md-6">
                     <label htmlFor="email" className="form-label">Email</label>
-                    <input {...register('email')} type="email" className="form-control" id="email" />
+                    <input {...register('email', { required: "*Email is required." })} type="email" className="form-control" id="email" placeholder="Enter your Email address" />
+                    <ErrorMessage
+                        errors={errors}
+                        name="email"
+                        render={({ message }) => <p className="error-message">{message}</p>}
+                    />
                 </div>
                 <div className="col-md-6">
                     <label htmlFor="number" className="form-label">Mobile Number</label>
-                    <input {...register('number')} type="text" className="form-control" id="number" />
+                    <input {...register('number', { required: "*Mobile number is required." })} type="text" className="form-control" id="number" placeholder="Enter your Mobile number" />
+                    <ErrorMessage
+                        errors={errors}
+                        name="number"
+                        render={({ message }) => <p className="error-message">{message}</p>}
+                    />
                 </div>
                 <div className="col-md-6">
                     <label htmlFor="birthplace" className="form-label">Birthplace</label>
-                    <input {...register('birthplace')} type="text" className="form-control" id="birthplace" />
+                    <input {...register('birthplace', { required: "*Birthplace is required." })} type="text" className="form-control" id="birthplace" placeholder="Enter your Birthplace" />
+                    <ErrorMessage
+                        errors={errors}
+                        name="birthplace"
+                        render={({ message }) => <p className="error-message">{message}</p>}
+                    />
                 </div>
-                {/* <div className="col-md-6">
-                    <label htmlFor="sex" className="form-label">Sex</label>
-                    <input {...register('sex')} type="text" className="form-control" id="sex" />
-                </div> */}
                 <div class="col-md-6">
                     <label htmlFor="validationCustom04" className="form-label">Sex</label>
                     <select {...register('sex')} class="form-select" id="validationCustom04" required>
@@ -76,19 +104,39 @@ export const RegistrationForm = ({ user, onSubmit }) => {
                 </div>
                 <div className="col-12">
                     <label htmlFor="address" className="form-label">Address</label>
-                    <input {...register('address')} type="text" className="form-control" id="address" placeholder="Enter your permanent address" />
+                    <input {...register('address', { required: "*Address is required." })} type="text" className="form-control" id="address" placeholder="Enter your permanent address" />
+                    <ErrorMessage
+                        errors={errors}
+                        name="address"
+                        render={({ message }) => <p className="error-message">{message}</p>}
+                    />
                 </div>
                 <div className="col-md-4">
                     <label htmlFor="province" className="form-label">Province</label>
-                    <input {...register('province')} type="text" className="form-control" id="province" placeholder="Enter your Province" />
+                    <input {...register('province', { required: "*Province is required." })} type="text" className="form-control" id="province" placeholder="Enter your Province" />
+                    <ErrorMessage
+                        errors={errors}
+                        name="province"
+                        render={({ message }) => <p className="error-message">{message}</p>}
+                    />
                 </div>
                 <div className="col-md-4">
                     <label htmlFor="city" className="form-label">City</label>
-                    <input {...register('city')} type="text" className="form-control" id="city" placeholder="Enter your City" />
+                    <input {...register('city', { required: "*City is required." })} type="text" className="form-control" id="city" placeholder="Enter your City" />
+                    <ErrorMessage
+                        errors={errors}
+                        name="city"
+                        render={({ message }) => <p className="error-message">{message}</p>}
+                    />
                 </div>
                 <div className="col-md-4">
                     <label htmlFor="zip" className="form-label">Zip</label>
-                    <input {...register('zip')} type="text" className="form-control" id="zip" />
+                    <input {...register('zip', { required: "*Zip is required." })} type="text" className="form-control" id="zip" placeholder="Enter zip code" />
+                    <ErrorMessage
+                        errors={errors}
+                        name="zip"
+                        render={({ message }) => <p className="error-message">{message}</p>}
+                    />
                 </div>
                 <div className="form-group">
                     <Button
@@ -96,7 +144,8 @@ export const RegistrationForm = ({ user, onSubmit }) => {
                         variant="contained"
                         style={{
                             backgroundColor: '#343a40',
-                            color: '#fff'
+                            color: '#fff',
+                            width: '100%'
                         }}
                         size="medium"
                         className={classes.button}
