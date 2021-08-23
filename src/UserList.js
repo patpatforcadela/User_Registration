3437a54cc231cc3aca1ee78c51fba6367b6e1d13
Patrick <ptrckxforcadela@gmail.com>
2021-08-23 23:49:10 +0800

    Added displayed fields in the user list

--- src/UserList.js
+++ src/UserList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { getUsers } from './api'
+import { FaEdit } from 'react-icons/fa';
 import './style.css'
 
 export const UserList = () => {
@@ -20,8 +21,12 @@ export const UserList = () => {
                     <thead>
                         <tr >
                             <th>First name</th>
+                            <th>Middle Name</th>
                             <th>Last Name</th>
                             <th>Sex</th>
+                            <th>Address</th>
+                            <th>Email</th>
+                            <th>Number</th>
                             <th>Action</th>
                         </tr>
                     </thead>
@@ -31,10 +36,14 @@ export const UserList = () => {
                                 return (
                                     <tr key={user._id}>
                                         <td>{user.firstName}</td>
+                                        <td>{user.middleName}</td>
                                         <td>{user.lastName}</td>
                                         <td>{user.sex}</td>
+                                        <td>{user.address}</td>
+                                        <td>{user.email}</td>
+                                        <td>{user.number}</td>
                                         <td>
-                                            <Link to={`/edit/${user._id}`} >Edit</Link>
+                                            <Link to={`/edit/${user._id}`} ><FaEdit style={{ color: '#343a40' }} /></Link>
                                         </td>
                                     </tr>
                                 )
